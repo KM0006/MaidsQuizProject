5eb30da242b58584657bac3f7fd7e0d8aa985fa2
Karam Moussa <karammoussa28@gmail.com>
2024-12-03 19:26:08 +0300

    Project Initiation

---
+++ src/main/java/bookstore/maidsquizproject/MaidsQuizProjectApplication.java
@@ -0,0 +1,15 @@
+package bookstore.maidsquizproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaidsQuizProjectApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MaidsQuizProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bookstore/maidsquizproject/MaidsQuizProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package bookstore.maidsquizproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaidsQuizProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
